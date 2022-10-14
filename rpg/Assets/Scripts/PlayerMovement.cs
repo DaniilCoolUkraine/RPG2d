c0bf00d759ff971ee0e11ea7649a15de72ee94f6
DaniilCoolUkraine <cd0games2000@gmail.com>
2022-10-14 20:20:53 +0300

    changed dash ready terms, refactored code(using functions)

--- rpg/Assets/Scripts/PlayerMovement.cs
+++ rpg/Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,27 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //player gameobject components
     private Rigidbody2D _rigidbody2D;
     private SpriteRenderer _playerSprite;
-    
     private BoxCollider2D _collider;
-    [SerializeField] private LayerMask _platformsLayer;
     
+    //variable to move player and store input 
     private Vector2 _moveDirection = Vector2.zero;
+    
     
     [SerializeField] private float speed;
     [SerializeField] private float jumpHeight;
     [SerializeField] private float dashDistance;
-    [SerializeField] private float dashCooldown;
-    private float currentDashCooldown;
-    private bool _dashReady = true;
+                     private bool _dashReady = true;
 
+    [Space(10)]
+    [Tooltip("Layer where player can stand and restore jump&dash")]
+    [SerializeField] private LayerMask platformsLayer;
+    
+    //player input class and its properties to store and read input from different devices
     private PlayerInput _playerControls;
     private InputAction _move;
     private InputAction _fire;
@@ -27,9 +30,11 @@ public class PlayerMovement : MonoBehaviour
     
     private void Awake()
     {
+        //getting components from player gameobject
         _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
         _playerSprite = gameObject.GetComponent<SpriteRenderer>();
         _collider = gameObject.GetComponent<BoxCollider2D>();
+        
         _playerControls = new PlayerInput();
     }
     
@@ -62,26 +67,42 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         _moveDirection = _move.ReadValue<Vector2>();
+        ChangeFlipState();
         
-        if (_moveDirection.x > 0)
-            _playerSprite.flipX = false;
-        if (_moveDirection.x < 0)
-            _playerSprite.flipX = true;
-
-        if (currentDashCooldown >= 0)
-        {
-            currentDashCooldown -= Time.deltaTime;
-        }
-        else
-        {
-            _dashReady = true;
-        }
+        ChangeDashStateToReady();
     }
 
     private void FixedUpdate()
     {
         _rigidbody2D.velocity = new Vector2(_moveDirection.x * speed, _rigidbody2D.velocity.y);
     }
+
+    //flip player facing its moving direction
+    private void ChangeFlipState()
+    {
+        if (_moveDirection.x > 0)
+            _playerSprite.flipX = false;
+        if (_moveDirection.x < 0)
+            _playerSprite.flipX = true;
+    }
+    
+    private void ChangeDashStateToReady()
+    {
+        if (IsGrounded())
+            _dashReady = true;
+    }
+    
+    private bool IsGrounded()
+    {
+        var bounds = _collider.bounds;
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(bounds.center, bounds.size, 0f, Vector2.down, .1f, platformsLayer);
+        
+        if (raycastHit2D.collider != null) 
+            return true;
+        return false;
+    }
+
+    #region playerInputActions
 
     private void Fire(InputAction.CallbackContext context)
     {
@@ -96,12 +117,6 @@ public class PlayerMovement : MonoBehaviour
             _rigidbody2D.AddForce(jump, ForceMode2D.Impulse);
         }
     }
-    private bool IsGrounded()
-    {
-        var bounds = _collider.bounds;
-        RaycastHit2D raycastHit2D = Physics2D.BoxCast(bounds.center, bounds.size, 0f, Vector2.down, .1f, _platformsLayer);
-        return raycastHit2D.collider != null;
-    }
 
     private void Dash(InputAction.CallbackContext context)
     {
@@ -115,8 +130,9 @@ public class PlayerMovement : MonoBehaviour
         {
             _rigidbody2D.AddForce(dash);
             
-            currentDashCooldown = dashCooldown;
             _dashReady = false;
         }
     }
+    
+    #endregion
 }
